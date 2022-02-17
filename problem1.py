bafcdf40c996f58778bdde73aef46b051c250868
Gabby <65522279+BobbyTie@users.noreply.github.com>
2022-02-17 14:44:40 +0000

    finished problem 1 and 2

--- problem1.py
+++ problem1.py
@@ -1,6 +1,17 @@
-#
-# Name:
-# Collaborator(s):
+# Name: Gabriella Laines
 #
 
+sec_input = int(input("How many seconds? "))
 
+seconds = sec_input
+
+minutes = sec_input // 60
+seconds = sec_input % 60
+
+hours = minutes // 60
+minutes = minutes % 60
+
+days = hours // 24
+hours = hours % 24
+
+print(f"In {sec_input} seconds, there are {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
--- problem2.py
+++ problem2.py
@@ -1,6 +1,23 @@
-#
-# Name:
-# Collaborator(s):
+# Name: Gabriella Laines
 #
 
+import math
 
+lat1 = float(input("What is the latitude of your first point? "))
+lon1 = float(input("What is the longitude of your first point? "))
+lat2 = float(input("What is the latitude of your second point? "))
+lon2 = float(input("What is the longitude of your second point? "))
+
+lat1 = math.radians(lat1)
+lon1 = math.radians(lon1)
+lat2 = math.radians(lat2)
+lon2 = math.radians(lon2)
+
+dlon = lon2-lon1
+dlat = lat2-lat1
+a = (math.sin(dlat/2))**2+math.cos(lat1) * math.cos(lat2) * (math.sin(dlon/2))**2
+c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+d_mi = 3956 * c
+d_km = 6367 * c
+
+print("The distance between the two points is", d_mi, "miles, or", d_km, "kilometers")
